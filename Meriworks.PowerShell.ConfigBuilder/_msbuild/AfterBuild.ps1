4ed03ee1b5b0128e8a7e8e79df017472cbf473b1
internal\dan <dan@meriworks.se>
2016-10-21 11:08:22 +0200

    Upgraded packages

--- Meriworks.PowerShell.ConfigBuilder/_msbuild/AfterBuild.ps1
+++ Meriworks.PowerShell.ConfigBuilder/_msbuild/AfterBuild.ps1
@@ -1,7 +1,5 @@
 ﻿param([string] $solutionDir, [string] $projectDir, [string] $targetPath)
 #trap {Write-Host -foreground red "Error occurred in AfterBuild.ps1: " $_.Exception.Message; exit 1; continue}
-#Add powershell statements that should be executed after the build
-. (Join-Path $projectDir "_msbuild/Meriworks.PowerShell.Sign/Functions.ps1")
 
 Write-Host "Signing scripts"
 SignScriptsInFolder (join-path $projectDir "nuspec/tools")
